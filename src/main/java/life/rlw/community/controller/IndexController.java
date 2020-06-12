40af4173e136ed0a3b82123b8986eb5447720388
rlw <598449595@qq.com>
2020-06-12 23:50:02 +0800

    question submit

--- src/main/java/life/rlw/community/controller/IndexController.java
+++ src/main/java/life/rlw/community/controller/IndexController.java
@@ -21,19 +21,16 @@ public class IndexController {
 //    访问首页时，循环获取到cookie，拿到token的值去数据库中查找，如果有把user放到index.html的session中
     public String index(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie:cookies)
-        {
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user!=null)
-                {
-                    request.getSession().setAttribute("user",user);
+        if(cookies!=null &&cookies.length!=0)
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
-        }
-
-
         return "index";}
 }
