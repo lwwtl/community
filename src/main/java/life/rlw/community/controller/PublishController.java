dc25b3d69a7e544b3b3ff8be2f2725dacda87244
lwwtl <598449595@qq.com>
2020-06-28 19:10:56 +0800

    show

--- src/main/java/life/rlw/community/controller/PublishController.java
+++ src/main/java/life/rlw/community/controller/PublishController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +17,16 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
     @Autowired(required = false)
     private QuestionMapper questionMapper;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id")Integer id,
+                                                Model model){
+        Question question = questionMapper.getById(id);
+        model.addAttribute("title",question.getTitle());
+        model.addAttribute("description",question.getDescription());
+        model.addAttribute("tag",question.getTag());
+        return "publish";
+    }
 
     @GetMapping("/publish")
     public String publish() {
@@ -29,6 +40,9 @@ public class PublishController {
             @RequestParam("tag") String tag,
             HttpServletRequest request,
             Model model) {
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
 
         if(title==null || title==""){
             model.addAttribute("error","标题不能为空");
