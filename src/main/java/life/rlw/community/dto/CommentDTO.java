6954c8ab6d4cc745d270948346a57ccb6e2a1f44
lwwtl <598449595@qq.com>
2020-08-09 14:53:34 +0800

    push

---
+++ src/main/java/life/rlw/community/dto/CommentDTO.java
@@ -0,0 +1,41 @@
+package life.rlw.community.dto;
+
+public class CommentDTO {
+    public Long getParentId() {
+        return parentId;
+    }
+
+
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+    @Override
+    public String toString() {
+        return "CommentDTO{" +
+                "parentId=" + parentId +
+                ", content='" + content + '\'' +
+                ", type=" + type +
+                '}';
+    }
+
+    private Long parentId;
+    private String content;
+    private Integer type;
+}
