1442d6d90201c452866f1dac66d12716ccd9ff16
lwwtl <598449595@qq.com>
2020-08-15 23:16:56 +0800

    second reply

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,5 +1,6 @@
 
-
+/**
+* 提交回复*/
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
@@ -36,4 +37,26 @@ function post() {
             console.log(response);
         }
     });
+}
+/**
+ * 二级评论*/
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-"+id);
+    //获取展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if(collapse){
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    }else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论的展开状态
+        e.setAttribute("data-collapse","in");
+        e.classList.add("active");
+
+    }
+
 }
